3024bb2dd5690ee990ca0c1169e849bb9a5649e0
Nicholas Patel <np4795@yahoo.com>
2020-02-15 19:56:34 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,8 +4,38 @@
 #   "Amanda" => 27
 # }
 
-def happy_birthday(birthday_kids)
-  # add your code snippet here!
+def select_winner(passengers)
+  
+  winner = ""
+  passengers.each do |suite,name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    end
+  end
+  winner
 end
 
 
+
+def happy_birthday(birthday_kids)
+  
+  birthday_kids.each do |kids_name,age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
+
+
+def age_appropriate_birthday(kids)
+   birthday_kids = {
+   "Timmy" => 9, 
+   "Sarah" => 6, 
+   "Amanda" => 27
+}
+  birthday_kids.each do |name, age|
+  if age < 12
+      puts "Happy Birthday #{name}! You are now #{age} years old!"
+    else
+      puts "You are too old for this."
+    end
+  end
+end
